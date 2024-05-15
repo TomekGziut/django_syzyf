544af432f106174a6ca815231ca28a394d568684
TomekGziut <u24_tomgzi_lbn@technischools.com>
2024-05-15 14:00:12 +0200

    usunąłem bo chciałem lepiej ale mi nie dali
    ojojojojojojoj bo biblioteka też kurcze kurcze kurcze chyba tracę chcęci do dalszego egzystowania w społeczeństwie i otaczającym mnie świecie

--- django 16.05.2024/django/cwiczenia_wprowadzajace/kalulator_sredniej/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class KalulatorSredniejConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kalulator_sredniej'
--- django 16.05.2024/django/cwiczenia_wprowadzajace/kalulator_sredniej/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('kalulator_sredniej/', kalulator_sredniej, name='kalulator_sredniej'),
-    path('process-kalulator_sredniej/', process_kalulator_sredniej, name='process_kalulator_sredniej'),
-    path('kalulator_sredniej/edit2/', edit2, name='edit2'),
-]
